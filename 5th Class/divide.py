0e6a2a873916f789309968418bb286b7cca29ca2
MehulPaliwalOfficial <mehulpaliofficial@gmail.com>
2026-09-19 22:33:25 +0530

    5th Class

---
+++ 5th Class/divide.py
@@ -0,0 +1,64 @@
+# print(type(5 + 4))
+# print(type(5 - 4))
+# print(type(5 * 4))
+# print(type(5 / 4))
+# print(type(5 % 4))
+
+# print(19 // 7)
+# print(int(19 // 7))
+# print(100 // 7)
+# print(8 // 0)
+
+# print(int(-19 / 7))
+# print(-19 // 7)
+
+# print(-23 // 7, 23/7, int(-23 / 7), int(-23 // 7))
+
+# print( 19 //  7)
+# print(-19 // -7)
+# print(-19 //  7)
+# print( 19 // -7)
+
+# print(-19 % 7)
+# print(19 % -7)
+# print(19 / -7, 19 % -7, 19 // -7)
+
+# x, y = -19, 7
+# print(x - int(x/y) * y)
+# print(x % y)
+
+# print(-23%10)
+
+# print(type(24 // 6))
+# print(type(24 // 6.0))
+
+# print(24 // 6)
+# print(24 // 6.0)
+
+# print(7.5 / 2)
+# print(7.5 // 2)
+# print(7.5 % 2)
+
+# print(450.0 % 360.0)
+# print(26.5 % 24.0)
+# print(3.75 % 1.0)
+
+# print(7.5 / 2)
+# print(7.5 // 2)
+# print(7.5 % 2)
+# print(-7.5 // 2)
+# print(-7.5 % 2)
+# print(-7.5 % 2)
+# print(7.5 % -2)
+# print(24 // 6.0)
+# print(-7 // 2)
+# print(5.5 % 2.0)
+
+# print(2 + 3 * 4)
+# print((2 + 3) * 4)
+
+# print(2 ** 3)
+# print(2 * 3 ** 2)
+# print(9/2 ** (2*2))
+
+# print(10 - 9 + 2 ** (16/4) // 3)
