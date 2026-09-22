ddd5467e9e5f25a1bd0ba35aa1accddb641fa9d8
MehulPaliwalOfficial <mehulpaliofficial@gmail.com>
2026-09-22 15:02:21 +0530

    Day 8

---
+++ 8th Class/String.py
@@ -0,0 +1,29 @@
+# names = "Python"
+# scores = [95, 87, 92]
+# # tags = {"Python", "AI", "Beginner"}
+# print(names[1])
+# print(scores[1])
+# # print(tags[1])
+
+# s = "hello" #Error because s is immutable i.e. cannot be edited
+# s[0] = "H"
+# print(s)
+
+# s = "hello"
+# s = "Hey"
+# print(s)
+
+# s = "hello"
+# s += "world"
+# print(s)
+
+# x = "Hey"
+# y = "There"
+# z = "How are ya"
+# print((id)(x), id(y), id(z))
+
+# m = "Mehul"
+# n = "Mehul"
+# o = "mehul"
+# print(id(m), id(n), id(o))
+
